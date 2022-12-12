5c43f3d8c66bbc72af43f58c82dad7752aca994f
Yunting Ma <yuntingm@usc.edu>
2022-12-12 11:05:06 +0000

    Initial commit for hw6

--- hash.h
+++ hash.h
@@ -20,15 +20,36 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
+			std::string temp = k;
+			unsigned long long w[5] = {0, 0, 0, 0, 0};
 
+			for (int j = 0; j < 5; j++) {
+				int substringStart = std::max(0, int(temp.size() - (j + 1) * 6));
+				int substringEnd = temp.size() - j * 6;
+				std::string substring = temp.substr(substringStart, substringEnd - substringStart);
+				for (int i = 0; i < (int)substring.size(); i++) {
+					w[j] += pow(36, i) * letterDigitToNumber(substring[i]);
+				}
+			}
 
+		 	HASH_INDEX_T result = 0;
+    	for (unsigned int i = 0; i < 5; i++) {
+				result += rValues[i] * w[i];
+    	}
+    	return result;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
-
+			if (letter >= 'a' && letter <= 'z') {
+				return letter - 'a';  
+			}
+			else if (letter >= '0' && letter <= '9') {
+				return letter - '0' + 26;
+			}
+			else return -1;
     }
 
     // Code to generate the random R values
@@ -47,3 +68,4 @@ struct MyStringHash {
 };
 
 #endif
+
